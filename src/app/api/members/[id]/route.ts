aebe0fc889032f1719a2677b00e16cd5615c235f
lewisthagichu <lewisthagichu254@gmail.com>
2024-10-14 09:46:18 +0300

    feat: Add getmember route handler

--- src/app/api/members/[id]/route.ts
+++ src/app/api/members/[id]/route.ts
@@ -1,6 +1,42 @@
 import prisma from '@/lib/db';
 import { revalidatePath } from 'next/cache';
 
+// GET /api/members/:id
+export const GET = async (
+  request: Request,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const id = params.id;
+
+    if (!id) {
+      return new Response('ID is required', { status: 400 });
+    }
+
+    const member = await prisma.member.findUnique({
+      where: { id },
+      include: {
+        nominee: true,
+        payments: true,
+        entranceFee: true,
+        monthlyContribution: true,
+        newPlantShares: true,
+      },
+    });
+
+    revalidatePath('/');
+    return new Response(JSON.stringify({ member }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    // Check if the error is an instance of Error
+    const errorMessage =
+      error instanceof Error ? error.message : 'An unexpected error occurred';
+    return new Response('error: ' + errorMessage, { status: 500 });
+  }
+};
+
 // DELETE /api/members/:id
 export const DELETE = async (
   request: Request,
@@ -35,7 +71,6 @@ export const DELETE = async (
     await prisma.member.delete({ where: { id } });
 
     revalidatePath('/');
-    console.log('hey');
     return new Response(JSON.stringify({ success: true }), {
       status: 200,
     });
